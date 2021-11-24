c3f8a9ed2bc38619532a525ebde74db4deb17fb3
Shir Fintsy <shir.fintsy@live.biu.ac.il>
2021-11-24 12:02:22 -0800

    create

--- server.py
+++ server.py
@@ -25,35 +25,35 @@ def get_random_id(id_set):
 
 
 def init_client_files(get_files_sock):
-        while True:
-            line = get_files_sock.readline()
-            if not line:
-                break  # no more files, client closed connection.
+    while True:
+        line = get_files_sock.readline()
+        if not line:
+            break  # no more files, client closed connection.
 
-            filename = line.strip().decode()
-            length = int(get_files_sock.readline())
-            print(f'Downloading {filename}...\n  Expecting {length:,} bytes...', end='', flush=True)
+        filename = line.strip().decode()
+        length = int(get_files_sock.readline())
+        print(f'Downloading {filename}...\n  Expecting {length:,} bytes...', end='', flush=True)
 
-            file_path = os.path.join('AllClients', str(client_id))
-            file_path = os.path.join(file_path, filename)
-            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        file_path = os.path.join('AllClients', str(client_id))
+        file_path = os.path.join(file_path, filename)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-            # Read the data in chunks so it can handle large files.
-            with open(file_path, 'wb') as f:
-                while length:
-                    chunk = min(length, CHUNKSIZE)
-                    data = get_files_sock.read(chunk)
-                    if not data:
-                        break
-                    f.write(data)
-                    length -= len(data)
-                else:  # only runs if while doesn't break and length==0
-                    print('Complete')
-                    continue
+        # Read the data in chunks so it can handle large files.
+        with open(file_path, 'wb') as f:
+            while length:
+                chunk = min(length, CHUNKSIZE)
+                data = get_files_sock.read(chunk)
+                if not data:
+                    break
+                f.write(data)
+                length -= len(data)
+            else:  # only runs if while doesn't break and length==0
+                print('Complete')
+                continue
 
-            # socket was closed early.
-            print('Incomplete')
-            break
+        # socket was closed early.
+        print('Incomplete')
+        break
 
 
 def send_files(on_socket, src_dir):
@@ -79,19 +79,69 @@ def send_files(on_socket, src_dir):
     print('Done.')
 
 
-def get_comp_num(c_id, c_comp):
+def create_dirs(d_path):
+    if not os.path.exists(d_path):
+        create_dirs(os.path.dirname(d_path))
+        os.mkdir(d_path)
+
+
+def get_file(on_socket, file_path):  # type - makefile('rb')
+    file_name = on_socket.readline()
+    length = int(on_socket.readline())
+    create_dirs(os.path.dirname(file_path))
+    with open(file_path, 'wb') as f:
+        while length:
+            chunk = min(length, CHUNKSIZE)
+            data = on_socket.read(chunk)
+            if not data:
+                break
+            f.write(data)
+            length -= len(data)
+        else:  # only runs if while doesn't break and length==0
+            print('Complete')
+
+
+def get_my_files(client_file, curr_path):  # type - makefile('rb')
+    while True:
+        line = client_file.readline()
+        if line.strip().decode() == "finito":
+            break  # no more files, client closed connection.
+
+        filename = line.strip().decode()
+        length = int(client_file.readline())
+        print(f'Downloading {filename}...\n  Expecting {length:,} bytes...', end='', flush=True)
+
+        file_path = os.path.join(curr_path, filename)
+        # os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        # Read the data in chunks so it can handle large files.
+        with open(file_path, 'wb') as f:
+            while length:
+                chunk = min(length, CHUNKSIZE)
+                data = client_file.read(chunk)
+                if not data:
+                    break
+                f.write(data)
+                length -= len(data)
+            else:  # only runs if while doesn't break and length==0
+                print('Complete')
+                continue
+
+        # socket was closed early.
+        print('Incomplete')
+        break
+    client_file.close()
+
+
+def get_comp_num(c_id):
     if data_base.keys().__contains__(c_id):  # check if the ID exists
-        if c_comp == "-1":  # check if the computer not exists
-            c_comp = str(len(data_base[c_id]) + 1)
-            data_base[c_id][c_comp] = None  # new computer has joined the data base.
-        else:
-            return c_comp  # else we don't change the client_comp
+        c_comp = str(len(data_base[c_id]) + 1)
+        data_base[c_id][c_comp] = None  # new computer has joined the data base.
     else:
-        c_id = get_random_id(clients_id_path.keys())  # create client id
         c_comp = "1"
         data_base[c_id] = {}
         data_base[c_id][c_comp] = None
-    return c_comp, c_id
+    return c_comp
 
 
 if __name__ == "__main__":
@@ -110,13 +160,14 @@ if __name__ == "__main__":
         # identification - get client's ID, comp number
         client_id = get_data_sock.readline().strip().decode()
         client_comp = get_data_sock.readline().strip().decode()
-        client_comp, real_id = get_comp_num(client_id, client_comp)  # also update the database
 
         client_dir_path = ''
         if client_id == 'False':
-            client_id = real_id
+            client_id = get_random_id(clients_id_path.keys())  # create client id
+            client_comp = get_comp_num(client_id)  # also update the database
             client_socket.sendall(client_id.encode('utf-8') + b'\n')
-            client_socket.sendall(client_comp.encode('utf-8') + b'\n')   # notify the client that he is the first computer of this ID.
+            client_socket.sendall(
+                client_comp.encode('utf-8') + b'\n')  # notify the client that he is the first computer of this ID.
 
             # create & enter his folder name to the dictionary
             os.mkdir(os.path.join(allClients, client_id))
@@ -128,17 +179,28 @@ if __name__ == "__main__":
         # ID exists:
         else:
             client_dir_path = clients_id_path[client_id]  # search for path in AllClients folders
+            if client_comp == "-1":
+                client_comp = get_comp_num(client_id)  # also update the database
+                client_socket.sendall(client_comp.encode('utf-8') + b'\n')
+                send_files(client_socket, client_dir_path)
 
-            # check if the computer is already on the system - we just added him hence the condition.
-            if str(len(data_base[client_id])) != client_comp:
+            # computer reconnect with id and comp number:
+            else:
                 # already signed computer- check if there are updates:
                 if data_base[client_id][client_comp]:  # check if there are some updates
                     updates = data_base[client_id][client_comp]  # list of updates
-                    cmd = get_data_sock.readline().strip().decode()
                     # TODO: share the updates to this computer
                 # waiting for updates
-            else:
-                client_socket.sendall(client_comp.encode('utf-8') + b'\n')
-                send_files(client_socket, client_dir_path)
+                command = get_data_sock.readline().strip().decode().split(',')
+                if command[0] == "created":
+                    is_dir, src_path = command[1], command[2]
+                    src_path = os.path.join(clients_id_path[client_id], src_path)
+                    if is_dir == "True" and not os.path.exists(src_path):
+                        os.mkdir(src_path)
+                        print("created dir")
+                    elif is_dir == "False":
+                        get_file(get_data_sock, src_path)
+
+
 
             # if it is not a new client, and not a new computer than the client needs something. (push or pull)
